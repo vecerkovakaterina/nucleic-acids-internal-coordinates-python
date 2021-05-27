7704b94f4ff9bf120c9cd1b6d29e5b331189adb6
Kateřina Večerková <36918524+KaterinaVecerkova@users.noreply.github.com>
2021-05-27 17:22:31 +0200

    Override theta function in cgdna

--- coordinates/coordinatesCgDNA.py
+++ coordinates/coordinatesCgDNA.py
@@ -1,7 +1,10 @@
-from coordinates.coordinates import Coordinates
 from coordinates.coordinatesCurves import CoordinatesCurves
+import numpy as np
 
 
-class CoordinatesCgDNA(Coordinates, CoordinatesCurves):
-    def run(self):
-        pass
+class CoordinatesCgDNA(CoordinatesCurves):
+
+    def get_theta(self, rotation_matrices):
+        theta_array = [np.arccos((np.trace(x) - 1) / 2) for x in rotation_matrices]
+        theta_array = [np.degrees(2 * np.tan(t / 2)) for t in theta_array]
+        return theta_array
