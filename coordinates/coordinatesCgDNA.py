c49b49762b84f0fc4feb170641dc882644476910
Kateřina Večerková <36918524+KaterinaVecerkova@users.noreply.github.com>
2021-06-01 18:14:12 +0200

    Add docstring to CoordinatescgDNA class

--- coordinates/coordinatesCgDNA.py
+++ coordinates/coordinatesCgDNA.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 class CoordinatesCgDNA(CoordinatesCurves):
+    """A class to represent internal coordinates according to the cgDNA definition."""
 
     def get_theta(self, rotation_matrices):
+        """Takes the array of rotation matrices.
+        Returns an array of rotation angles."""
         theta_array = [np.arccos((np.trace(x) - 1) / 2) for x in rotation_matrices]
         theta_array = [np.degrees(2 * np.tan(t / 2)) for t in theta_array]
         return theta_array
